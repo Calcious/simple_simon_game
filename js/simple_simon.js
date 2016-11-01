265644e454abecd7738626b8228f385f42782e80
Justin Armer <justin.armer@gmail.com>
2016-11-01 11:30:24 -0500

    Heading a new direction--cleared functions out that weren't working//Combined random square selector function AND the animate function

--- js/simple_simon.js
+++ js/simple_simon.js
@@ -16,24 +16,26 @@
     var gameArray = [];
     var userArray = [];
 
-//Adding event listeners to buttons
+//Adding event listeners to buttons on users turn
 
-    $('#tL').click(function(){
+function userInput() {
+    $('#tL').click(function () {
         console.log("1");
         userArray.push("1");
     });
-    $('#tR').click(function(){
+    $('#tR').click(function () {
         console.log("2");
         userArray.push("2");
     });
-    $('#bL').click(function(){
+    $('#bL').click(function () {
         console.log("3");
         userArray.push("3");
     });
-    $('#bR').click(function(){
+    $('#bR').click(function () {
         console.log("4");
         userArray.push("4");
     });
+}
 
     //Hiding the amount of rounds and round status until game begins (will be added to HTML in the future)
     //$('#roundNumber').hide()
@@ -45,75 +47,62 @@
 //clearInterval(id_of_setinterval)
 
 // This happens when the "begin game" button is selected
-    function startGame(){
-    var roundCount = 0;
+    $('#startButt').click(function startGame(){
     gameArray.length = 0;
         compTurn();
-        roundCount++;
-    }
+    });
 
 
-//A function that takes a game button as a parameter and animates it when selected by either comp or player
+//A function that follows the path of the comp's turn
     function compTurn(){
         sequenceGen();
-        var i = 0;
-        var intervalId = setInterval(function(){
-            animateSeq(i);
-            i++;
-            if (i >= gameArray.length) {
-                clearInterval(intervalId);
-            }
-        },1500);
     }
 
 
 
-// This generates a random sequence
+// This generates a random sequence and animates the outcome
     function sequenceGen() {
         var randomSquare = Math.floor(Math.random() * 4) + 1;
         switch (true) {
                     case (randomSquare == 1):
-                        gameArray.push('tL');
+                        gameArray.push('1');
+                        console.log("sequenceGen just pushed 1");
+                        boxAnim('#tL');
                         break;
                     case (randomSquare == 2):
-                        gameArray.push('tR');
+                        gameArray.push('2');
+                        console.log("sequenceGen just pushed 2");
+                        boxAnim('#tR');
                         break;
                     case (randomSquare == 3):
-                        gameArray.push('bL');
+                        gameArray.push('3');
+                        console.log("sequenceGen just pushed 3");
+                        boxAnim('#bL');
                         break;
                     case (randomSquare == 4):
-                        gameArray.push('bR');
+                        gameArray.push('4');
+                        console.log("sequenceGen just pushed 4");
+                        boxAnim('#bR');
                         break;
                 }
     }
 
 //Tells the user that the game is over
     function gameOver(){
-        userArray = 0;
         gameArray.length = 0;
         alert("Game Over--seems Simple Simon isn't so simple")
     }
 
 
-//This happens when the comp selects a series of boxes
+//This happens when the comp animates boxes
     function boxAnim(boxid) {
-        $(boxid).animate({width: 100}, 100);
-        $(boxid).animate({height: 100}, 100);
+        $(boxid).fadeOut(300).fadeIn(300);
+
     }
 
+    //A function that compares the user input to compTurn
 
-// A function that causes an animation in the boxes as they are selected
-    function animateSeq(i) {
-        if (gameArray[i] == 'tL')  {
-            boxAnim('#topLeft');
-        } else if (sgameArray[i] == 'tR')  {
-            boxAnim('#topRight');
-        } else if (gameArray[i] == 'bL')  {
-            boxAnim('#bottomLeft');
-        } else if (gameArray[i] == 'bR')  {
-            boxAnim('#bottomRight');
-        };
-    }
+
 
 
 
