3c36b4fcf6a1b050e45a375cc0e98407e05ca912
Justin Armer <justin.armer@gmail.com>
2016-10-31 20:58:29 -0500

    Added JS to-do list via comments

--- js/simple_simon.js
+++ js/simple_simon.js
@@ -12,30 +12,74 @@
     $('#instructions').hide().slideDown(2000);
 
 
-    // var gameArray= [];
 
+    var gameArray = [];
+    var userArray = [];
+
+//Adding event listeners to buttons
+
+    $('#tL').click(function(){
+        console.log("1");
+        userArray.push("1");
+    });
+    $('#tR').click(function(){
+        console.log("2");
+        userArray.push("2");
+    });
+    $('#bL').click(function(){
+        console.log("3");
+        userArray.push("3");
+    });
+    $('#bR').click(function(){
+        console.log("4");
+        userArray.push("4");
+    });
+
+    //Hiding the amount of rounds and round status until game begins (will be added to HTML in the future)
+    //$('#roundNumber').hide()
+    //$('#roundStatus').hide()
+
+//Methods that will be needed later--see W3schools for more information
+//setTimeout(function,milliseconds,param1,param2,...)
+//setInterval(function,milliseconds,param1,param2,...)
+//
 
 //This happens when the "begin game" button is selected
 //     function startGame(){
-//
+//roundCount = 0
 //     }
 
 
+//A function that takes a game button as a parameter and animates it when selected by either comp or player
+    //function boxAnim(){}
+
+//This generates a random sequence
+//     var randomSeq =
+
+//This adds an event listener to console.log a value assigned to each button (1, 2, 3, & 4)
+
+//Tells the user that the game is over
+    //$('#roundNumber').hide()
+    //$('#roundStatus').hide()
+    //roundCount = 0
 
 
-    // function buttonAnim(buttId){
-    //     $(buttId).animate(
-    //         {opacity: 1
-    //         }, 100);
-    //     $(buttId).animate(
-    //         {borderWidth: 1
-    //         }, 100);
-    //     $(buttId).animate(
-    //         {opacity: .5
-    //         }, 100);
-    //     $(buttId).animate(
-    //         {borderWidth: 0
-    //         }, 100);
-    // }
+//This happens when the comp selects a series of boxes
+
+
+// A function that causes an animation in the boxes as they are selected
+//     function ???(i) {
+//         if (sequence[i] == 'tL')  {
+//             boxAnim('#topLeft');
+//         } else if (sequence[i] == 'tR')  {
+//             boxAnim('#topRight');
+//         } else if (sequence[i] == 'bL')  {
+//             boxAnim('#bottomLeft');
+//         } else if (sequence[i] == 'bR')  {
+//             boxAnim('#bottomRight');
+//         };
+//     }
+
+
 
 }());
